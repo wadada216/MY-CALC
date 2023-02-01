e87db62a202bbc987f03dc2bb65a705911c42294
joshua <jd_lorenzo.student@ua.edu.ph>
2023-02-01 17:10:43 +0800

    Added CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,39 @@ import { useState } from 'react';
 
 function Button({btnLabel, onClick}){
   return (
-    <button onClick={onClick}>
+    <button class="btnNum"onClick={onClick}>
+      {btnLabel}
+    </button>
+  );
+}
+
+function ButtonOp({btnLabel, onClick}){
+  return (
+    <button class="btnOp" onClick={onClick}>
+      {btnLabel}
+    </button>
+  );
+}
+
+function ButtonPlus({btnLabel, onClick}){
+  return (
+    <button class="btnPlus" onClick={onClick}>
+      {btnLabel}
+    </button>
+  );
+}
+
+function ButtonClear({btnLabel, onClick}){
+  return (
+    <button class="btnClr" onClick={onClick}>
+      {btnLabel}
+    </button>
+  );
+}
+
+function ButtonEqual({btnLabel, onClick}){
+  return (
+    <button class="btnEq" onClick={onClick}>
       {btnLabel}
     </button>
   );
@@ -11,7 +43,7 @@ function Button({btnLabel, onClick}){
 
 function DisplayText({text}) {
   return (
-    <h1>{text}</h1>
+    <div>{text}</div>
   );
 }
 
@@ -19,54 +51,88 @@ function DisplayText({text}) {
 function App() {
 
   const [disp, setDisp] = useState("0");
+  const [num1, setNum1] = useState("null");
+  const [num2, setNum2] = useState("0");
+  const [oper, setOper] = useState("null");
 
   const numberClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-    console.log(value);
-    setDisp(value);
+
+    if(oper === null){
+      setNum1(num1+value);
+      setDisp(disp+value);
+    }else{
+      setNum2(num1+value);
+      setDisp(disp+value);
+    }
+    // console.log(value);
+    // setNum1(value);
+    // setDisp(value);
+    // console.log(num1);
+    // console.log(disp)
+    // console.log(value)
+    // console.log((value)+num*10);
     // alert(value)
+    console.log(num1);
+    console.log(disp);
+  };
+  
+  const clearClickHandler = () => {
+    setNum1("null");
+    setNum2("null");
+    setDisp("0");
+
+    console.log(disp);
+    console.log(num1);
+    console.log(num2);
   };
 
 
   return (
-    <div class="main-container-calc">
-      <div class="displayVal">
-      <DisplayText text={disp}/>
-      </div>
-      <div>
-        <section>
-          <Button btnLabel="7" onClick={numberClickHandler}/>
-          <Button btnLabel="8" onClick={numberClickHandler}/>
-          <Button btnLabel="9" onClick={numberClickHandler}/>
-          <Button btnLabel="/" onClick={numberClickHandler}/>
-        </section>
-      </div>
-      <div>
-        <section>
-          <Button btnLabel="4" onClick={numberClickHandler}/>
-          <Button btnLabel="5" onClick={numberClickHandler}/>
-          <Button btnLabel="6" onClick={numberClickHandler}/>
-          <Button btnLabel="x" onClick={numberClickHandler}/>
-        </section>
-      </div>
-      <div>
-        <section>
-          <Button btnLabel="1" onClick={numberClickHandler}/>
-          <Button btnLabel="2" onClick={numberClickHandler}/>
-          <Button btnLabel="3" onClick={numberClickHandler}/>
-          <Button btnLabel="-" onClick={numberClickHandler}/>
-        </section>
-      </div>
-      <div>
-        <section>
-          <Button btnLabel="clr" onClick={numberClickHandler}/>
-          <Button btnLabel="0" onClick={numberClickHandler}/>
-          <Button btnLabel="=" onClick={numberClickHandler}/>
-          <Button btnLabel="+" onClick={numberClickHandler}/>
-        </section>
-      </div>
+    <div class="container">
+      <div class="main-container-calc">
 
+        <div class="displayVal">
+        <DisplayText text={disp}/>
+        </div>
+
+        <div class="buttonsDisplay">
+          <div>
+            <section>
+              <Button btnLabel="7" onClick={numberClickHandler}/>
+              <Button btnLabel="8" onClick={numberClickHandler}/>
+              <Button btnLabel="9" onClick={numberClickHandler}/>
+              <ButtonOp btnLabel="÷" onClick={numberClickHandler}/>
+            </section>
+          </div>
+          <div>
+            <section>
+              <Button btnLabel="4" onClick={numberClickHandler}/>
+              <Button btnLabel="5" onClick={numberClickHandler}/>
+              <Button btnLabel="6" onClick={numberClickHandler}/>
+              <ButtonOp btnLabel="x" onClick={numberClickHandler}/>
+            </section>
+          </div>
+          <div>
+            <section>
+              <Button btnLabel="1" onClick={numberClickHandler}/>
+              <Button btnLabel="2" onClick={numberClickHandler}/>
+              <Button btnLabel="3" onClick={numberClickHandler}/>
+              <ButtonOp btnLabel="-" onClick={numberClickHandler}/>
+            </section>
+          </div>
+          <div>
+            <section>
+              <ButtonClear btnLabel="clr" onClick={clearClickHandler}/>
+              <Button btnLabel="0" onClick={numberClickHandler}/>
+              <ButtonEqual btnLabel="=" onClick={numberClickHandler}/>
+              <ButtonPlus btnLabel="+" onClick={numberClickHandler}/>
+            </section>
+          </div>
+        </div>
+
+      </div>
     </div>
 
   );
